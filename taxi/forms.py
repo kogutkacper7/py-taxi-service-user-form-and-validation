e1e5a59980ed300f3d699990c150c3da01a5d848
Kacper <kogutkacper7@gmail.com>
2026-03-16 14:37:47 +0100

    Solution

---
+++ taxi/forms.py
@@ -0,0 +1,55 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
+
+from taxi.models import Driver, Car
+
+
+class DriverLicenseUpdateForm(forms.ModelForm):
+    MIN_LENGTH_LICENSE_NUMBER = 8
+    class Meta:
+        model = Driver
+        fields = ("license_number",)
+
+    def clean_license_number(self):
+        license_number = self.cleaned_data["license_number"]
+        if len(license_number) > DriverLicenseUpdateForm.MIN_LENGTH_LICENSE_NUMBER or len(license_number) < DriverLicenseUpdateForm.MIN_LENGTH_LICENSE_NUMBER:
+            raise ValidationError("License number must have only 8 characters!")
+
+        if not (license_number[:3].isalpha() and license_number[:3].isupper()):
+            raise ValidationError("Three first 3 characters must be uppercase and be letters")
+
+        if not license_number[3:].isdigit():
+            raise ValidationError("Last 5 characters must be digits!")
+
+        return license_number
+
+
+class DriverCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = Driver
+        fields = UserCreationForm.Meta.fields + ("license_number", "first_name", "last_name")
+
+    def clean_license_number(self):
+        license_number = self.cleaned_data["license_number"]
+        if len(license_number) > DriverLicenseUpdateForm.MIN_LENGTH_LICENSE_NUMBER or len(license_number) < DriverLicenseUpdateForm.MIN_LENGTH_LICENSE_NUMBER:
+            raise ValidationError("License number must have  8 characters!")
+
+        if not (license_number[:3].isalpha() and license_number[:3].isupper()):
+            raise ValidationError("Three first 3 characters must be uppercase and be letters")
+
+        if not license_number[3:].isdigit():
+            raise ValidationError("Last 5 characters must be digits!")
+
+        return license_number
+
+
+class CarForm(forms.ModelForm):
+    drivers = forms.ModelMultipleChoiceField(
+        queryset=Driver.objects.all(),
+        widget=forms.CheckboxSelectMultiple
+    )
+
+    class Meta:
+        model = Car
+        fields = "__all__"
--- taxi/urls.py
+++ taxi/urls.py
@@ -13,6 +13,10 @@ from .views import (
     ManufacturerCreateView,
     ManufacturerUpdateView,
     ManufacturerDeleteView,
+    DriverCreateView,
+    DriverDeleteView,
+    DriverUpdateView,
+    toggle_car_assignment
 )
 
 urlpatterns = [
@@ -46,6 +50,10 @@ urlpatterns = [
     path(
         "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
     ),
+    path("drivers/create", DriverCreateView.as_view(), name="driver-create"),
+    path("drivers/<int:pk>/update/", DriverUpdateView.as_view(), name="driver-update"),
+    path("drivers/<int:pk>/delete/", DriverDeleteView.as_view(), name="driver-delete"),
+    path("drivers/<int:pk>/assign", toggle_car_assignment, name="driver-assign")
 ]
 
 app_name = "taxi"
